b21d10551f0ad9724460e880a0d7d5ee67711d6e
hoang_nguyen <nguyenhoang692004@gmail.com>
2024-01-26 16:46:12 +0700

    Hoang-sua giao dien

--- Form1.cs
+++ Form1.cs
@@ -19,18 +19,14 @@ namespace Test_Git_01
 
         private void Form1_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-            //Hong Phuc Sua Form
+            //Hoang chinh sua giao dien
+
             //Hong Phuc update
-=======
             //Duong sua form
             //Hong phuc sua form
             //KhanhDuong sua form
-<<<<<<< HEAD
             //lan2
-=======
->>>>>>> 3c79ccdcbcd0a7779f9b2c622202f104a8b7afab
->>>>>>> e9389699b0d99468f075c47e663863f532b2673f
+
         }
     }
 }
