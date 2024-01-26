4aee966872757dda0797eace3b93b6d68c9967aa
Nguyen Dinh Hong Phuc <phuc11012904@gmail.com>
2024-01-26 10:57:52 +0700

    Hong Phuc tao form moi

---
+++ HongPhucForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Git_01
+{
+    public partial class HongPhucForm : Form
+    {
+        public HongPhucForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HongPhucForm_Load(object sender, EventArgs e)
+        {
+            //Hong Phuc Them Form
+        }
+    }
+}
